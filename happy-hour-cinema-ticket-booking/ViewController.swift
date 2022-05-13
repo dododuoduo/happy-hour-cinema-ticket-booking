e77872e38cdc2a4f93de1fccd3cad963ca15d3f3
Yihan <zenial069@gmail.com>
2022-05-13 15:02:04 +1000

    Initial Commit

---
+++ happy-hour-cinema-ticket-booking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  happy-hour-cinema-ticket-booking
+//
+//  Created by Yihan Liu on 13/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
