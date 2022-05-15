a74a41981782be82db31ea9011875cfc81e1b292
Yihan <zenial069@gmail.com>
2022-05-16 01:50:09 +1000

    Seats and cinema room completed
    TODO: connect to db and perform updates

---
+++ happy-hour-cinema-ticket-booking/Model/CinemaRoom.swift
@@ -0,0 +1,148 @@
+//
+//  CinemaRoom.swift
+//  happy-hour-cinema-ticket-booking
+//
+//  Created by Yihan Liu on 15/5/2022.
+//
+
+import Foundation
+import UIKit
+
+struct Pos {
+    var x: Int
+    var y: Int
+}
+
+class CinemaRoom {
+//    var seats: [Seat]
+    var seats: [Seat]
+    var seatPos: [[Pos]]
+    var superView: UIView
+    var width: Int
+    var height: Int
+    let seatsRow: Int = 5
+    let seatsCol: Int = 8
+    let seatSize: Int = 27
+    let seatGap: Int = 3
+    var selectedSeats: [String] = []
+    let reservedSeatsId: [String]
+    
+    init (superView: UIView, reservedSeatsId: [String]) {
+        self.superView = superView
+        self.reservedSeatsId = reservedSeatsId
+        self.seats = []
+        self.seatPos = []
+        self.width = Int(self.superView.frame.width)
+        self.height = Int(self.superView.frame.height)
+    }
+    
+    func markReservedSeats() {
+        print("Reserved seats: ", self.reservedSeatsId)
+        for seatId in self.reservedSeatsId {
+            for seat in seats {
+                if seat.id == seatId {
+                    seat.markAsReserved()
+                }
+            }
+        }
+    }
+    
+    func generateSeatPos() {
+        for r in 0..<self.seatsRow + 1 {
+            if r == 2 {
+                continue
+            }
+            
+            var posRow:[Pos] = []
+            
+            for c in 0..<self.seatsCol + 2 {
+                if c == 2 || c == 7 || r == 2 {
+                    continue
+                }
+                let seatX = c * (self.seatSize + self.seatGap)
+                let seatY = r * (self.seatSize + self.seatGap)
+                posRow.append(Pos(x: seatX, y: seatY))
+            }
+            
+            self.seatPos.append(posRow)
+        }
+    }
+    
+    @IBAction func seatTapped(_ sender: Seat) {
+        let selectedSeatId = sender.selectSeat()
+        self.selectedSeats.append(selectedSeatId)
+        print("Selected seats: ", self.selectedSeats)
+    }
+    
+    // generate a seat array
+    @objc func generateSeats() {
+        self.generateSeatPos()
+        for r in 0..<self.seatPos.count {
+            for c in 0..<self.seatPos[0].count {
+                let pos = self.seatPos[r][c]
+                let seatId = generateSeatId(r: r, c: c)
+                let seat = Seat(x: pos.x, y: pos.y, size: self.seatSize, id: seatId)
+                seat.addTarget(self, action: #selector(seatTapped(_:)), for: .touchUpInside)
+                self.seats.append(seat)
+            }
+        }
+    }
+    
+    // translate 2D coordinates of the seat to a 1D identifier
+    func generateSeatId(r: Int, c: Int)-> String{
+        var rowChar = ""
+        let colChar = String(c)
+        switch r {
+        case 0:
+            rowChar = "A"
+        case 1:
+            rowChar = "B"
+        case 2:
+            rowChar = "C"
+        case 3:
+            rowChar = "D"
+        case 4:
+            rowChar = "E"
+        default:
+            rowChar = "?"
+        }
+        return rowChar + colChar
+    }
+    
+    func renderSeats() {
+        self.generateSeats()
+        self.markReservedSeats()
+        
+        // create a container for all seats
+        let seatsContainerWidth = (self.seatSize + self.seatGap) * (self.seatsCol + 2) - self.seatGap
+        let seatsContainerHeight = (self.seatSize + self.seatGap) * (self.seatsRow + 1) - self.seatGap
+        let seatsWrapperView = UIView()
+        seatsWrapperView.frame.size.width = CGFloat(seatsContainerWidth)
+        seatsWrapperView.frame.size.height = CGFloat(seatsContainerHeight)
+        
+        // add all seats to the container
+        for seat in self.seats {
+            seatsWrapperView.addSubview(seat)
+        }
+        
+        self.superView.addSubview(seatsWrapperView)
+        
+        // center the container inside the parent view
+        seatsWrapperView.center = CGPoint(
+            x: seatsWrapperView.superview!.bounds.width / 2.0,
+            y: seatsWrapperView.superview!.bounds.height / 2.0
+        )
+        
+//        print("Parent width: ", seatsWrapperView.superview!.bounds.width)
+//        print("Parent height: ", seatsWrapperView.superview!.bounds.height)
+//        print("Parent center: ", seatsWrapperView.center)
+//
+//        print("Child width: ", seatsWrapperView.frame.width)
+//        print("Child height: ", seatsWrapperView.frame.height)
+//        print("Child center: ", seatsWrapperView.center)
+//
+//        self.superView.backgroundColor = .red
+//        seatsWrapperView.backgroundColor = .brown
+    }
+    
+}
---
+++ happy-hour-cinema-ticket-booking/Model/Seat.swift
@@ -0,0 +1,39 @@
+//
+//  Seat.swift
+//  happy-hour-cinema-ticket-booking
+//
+//  Created by Yihan Liu on 15/5/2022.
+//
+
+import Foundation
+import UIKit
+
+class Seat: UIButton {
+    var id: String
+    var size: Int
+    
+    required init(x: Int, y: Int, size: Int, id: String) {
+        self.id = id
+        self.size = size
+        let buttonFrame = CGRect(x: x, y: y, width: self.size, height: self.size)
+        super.init(frame: buttonFrame)
+        self.backgroundColor = UIColor(red: 72/255, green: 219/255, blue: 87/255, alpha: 1)
+        self.layer.cornerRadius = 3
+        self.setTitle(self.id, for: .normal)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init is not implemented!")
+    }
+    
+    public func selectSeat() -> String {
+        self.backgroundColor = .blue
+        self.isEnabled = false
+        return self.id
+    }
+    
+    public func markAsReserved() {
+        self.backgroundColor = UIColor(red: 255/255, green: 0/255, blue: 50/255, alpha: 1)
+        self.isEnabled = false
+    }
+}
