3b2eeedcab94cecc6b1d9f5583fd2bbcbb37dac3
Yihan <zenial069@gmail.com>
2022-05-18 21:17:35 +1000

    Order details completed

--- happy-hour-cinema-ticket-booking/Controller/MovieViewController.swift
+++ happy-hour-cinema-ticket-booking/Controller/MovieViewController.swift
@@ -14,6 +14,13 @@ class MovieViewController: UIViewController {
     @IBOutlet weak var myTicketButton: UIButton!
     @IBOutlet weak var logoutButton: UIButton!
     
+    @IBOutlet weak var doctorStrangeButton: UIButton!
+    @IBOutlet weak var sonicButton: UIButton!
+    
+    var selectedMovieId: String?
+    var selectedMovieName: String?
+    var selectedMovieDescription: String?
+    
     var db = DB()
     
     override func viewDidLoad() {
@@ -53,6 +60,35 @@ class MovieViewController: UIViewController {
             return
         }
         self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    @IBAction func onDoctorStrangeTapped(_ sender: Any) {
+        self.selectedMovieId = "doctor-strange-id"
+        self.selectedMovieName = "Doctor Strange in the Multiverse of Madness"
+        self.selectedMovieDescription = "In Marvel Studios’ Doctor Strange in the Multiverse of Madness, the MCU unlocks the Multiverse and pushes its boundaries further than ever before. Journey into the unknown with Doctor Strange traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary."
+        self.navToOrderDetails()
+    }
+    
+    @IBAction func onSonicTapped(_ sender: Any) {
+        self.selectedMovieId = "sonic-id"
+        self.selectedMovieName = "Sonic the Hedgehog 2"
+        self.selectedMovieDescription = "The world’s favorite blue hedgehog is back for a next-level adventure in SONIC THE HEDGEHOG 2. After settling in Green Hills, Sonic is eager to prove he has what it takes to be a true hero. His test comes when Dr. Robotnik returns, this time with a new partner, Knuckles, in search for an emerald that has the power to destroy civilizations. "
+        self.navToOrderDetails()
+    }
+    
+    @objc func navToOrderDetails() {
+        self.performSegue(withIdentifier: "movieToOrderDetails", sender: nil)
+    }
+    
+    // pass booking id to the order details controller
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "movieToOrderDetails") {
+            if let destinationVC = segue.destination as? OrderDetailsViewController {
+                destinationVC.movieId = self.selectedMovieId
+                destinationVC.movieName = self.selectedMovieName
+                destinationVC.moiveDescription = self.selectedMovieDescription
+            }
+        }
     }
     
 }
--- happy-hour-cinema-ticket-booking/Controller/OrderDetailsViewController.swift
+++ happy-hour-cinema-ticket-booking/Controller/OrderDetailsViewController.swift
@@ -9,8 +9,95 @@ import UIKit
 
 class OrderDetailsViewController: UIViewController {
     
+    
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var movieDescriptionLabel: UILabel!
+    @IBOutlet weak var ticketNumberDisplay: UILabel!
+    @IBOutlet weak var removeOneTicketButton: UIButton!
+    @IBOutlet weak var addOneTicketButton: UIButton!
+    
+    @IBOutlet weak var selectSeatsButton: UIButton!
+    
+    // input
+    var movieId: String?
+    var movieName: String?
+    var moiveDescription: String?
+    
+    // output
+    var numOfTicket: Int = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("OrderDetailsViewController works!")
+        initOrderDetailsView()
     }
+    
+    func initOrderDetailsView() {
+        Style.styleFilledButton(self.selectSeatsButton)
+        Style.styleHollowButton(self.addOneTicketButton)
+        Style.styleHollowButton(self.removeOneTicketButton)
+        self.numOfTicket = 1
+        self.ticketNumberDisplay.text = String(self.numOfTicket)
+        self.movieNameLabel.text = self.movieName!
+        self.movieDescriptionLabel.text = self.moiveDescription!
+    }
+    
+    @IBAction func onRemoveOneTicket(_ sender: Any) {
+        if self.numOfTicket <= 1 {
+            self.showAlert(msg: "Please order at least one ticket!")
+            return
+        }
+        
+        self.numOfTicket -= 1
+        self.ticketNumberDisplay.text = String(self.numOfTicket)
+
+    }
+    
+    @IBAction func onAddOneTicket(_ sender: Any) {
+        if self.numOfTicket >= 4 {
+            self.showAlert(msg: "Cannot order more than 4 tickets!")
+            return
+        }
+        
+        self.numOfTicket += 1
+        self.ticketNumberDisplay.text = String(self.numOfTicket)
+    }
+    
+    func showAlert(msg: String) {
+        let alert = UIAlertController(title: "Error", message: msg, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func isValidTicketNumber(ticketNum: Int)-> Bool {
+        if ticketNum <= 4 && ticketNum >= 1 {
+            return true
+        }
+        return false
+    }
+    
+    @IBAction func onSelectSeatsTapped(_ sender: Any) {
+        guard isValidTicketNumber(ticketNum: self.numOfTicket) else {
+            showAlert(msg: "Invalid ticket number! Valid range: 1-4")
+            return
+        }
+        
+        self.navToOrderConfirmation()
+    }
+    
+    @objc func navToOrderConfirmation() {
+        self.performSegue(withIdentifier: "orderToSeats", sender: nil)
+    }
+    
+    // pass data to the seats view controller
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "orderToSeats") {
+            if let destinationVC = segue.destination as? SeatsViewController {
+                destinationVC.movieName = self.movieName!
+                destinationVC.movieId = self.movieId!
+                destinationVC.maxSeatNum = self.numOfTicket
+            }
+        }
+    }
+    
 }
