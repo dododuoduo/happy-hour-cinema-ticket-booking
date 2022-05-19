2d9de2e7128150637862de392c67c24cea34b119
Yihan <zenial069@gmail.com>
2022-05-19 18:19:42 +1000

    My tickets & cancel booking works

--- happy-hour-cinema-ticket-booking/Controller/TicketViewController.swift
+++ happy-hour-cinema-ticket-booking/Controller/TicketViewController.swift
@@ -10,42 +10,119 @@ import UIKit
 import FirebaseAuth
 
 class TicketViewController: UIViewController {
+    
+    var scrollView: UIScrollView = UIScrollView()
+    var scrollViewContainer: UIStackView = UIStackView()
      
     var bookings: [Booking] = []
     let db = DB()
     let uid: String = Auth.auth().currentUser!.uid
     
-    // TODO: remove this button, ths button is just for test
-    @IBOutlet weak var testButton: UIButton!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("TicketViewController works!")
         
-        // a demo of how to get all bookings (for the current user) from the database
         self.db.getBookingByUid(uid: self.uid) { bookings in
             self.bookings = bookings
-            // if you want to do something RIGHT AFTER update bookings (e.g. make a function call to initTicketView())
-            // you should add code INSIDE the <{bookings in ... }> BLOCK:
             self.initTicketView()
         }
     }
     
-    
-    // TODO: this is an entry point of all your code, add your code to this function
     func initTicketView() {
         // Uid and bookings are ready to use
         // If no booking for the user, the bookings array is empty: []
-        print("Uid: \(self.uid)")
-        print("Bookings: \(self.bookings)")
-
+        self.initScrollView()
+        self.initScrollViewContainer()
+        self.initScrollViewItems()
         
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        scrollViewContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        scrollViewContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        scrollViewContainer.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        scrollViewContainer.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        // this is important for scrolling
+        scrollViewContainer.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
     }
+    
+    
+    func initScrollView() {
+        for sv in self.scrollView.subviews {
+            sv.removeFromSuperview()
+        }
+        
+        self.scrollView = UIScrollView()
+        self.scrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.scrollView.isScrollEnabled = true
+        self.view.addSubview(scrollView)
+    }
+    
+    
+    func initScrollViewContainer() {
+        self.scrollViewContainer = UIStackView()
+        self.scrollViewContainer.axis = .vertical
+        self.scrollViewContainer.spacing = 10
+        self.scrollViewContainer.translatesAutoresizingMaskIntoConstraints = false
+        self.scrollView.addSubview(self.scrollViewContainer)
+    }
+    
+    
+    func initScrollViewItems() {
+        for (i, booking) in bookings.enumerated() {
+            print("Index: \(i), booking: \(booking)")
+            let itemView = self.generateBookingItemView(booking: booking)
+            self.scrollViewContainer.addArrangedSubview(itemView)
+        }
+    }
+    
+    func generateBookingItemView(booking: Booking) -> UIStackView {
+        let itemView = UIStackView()
+        itemView.spacing = 5
+        itemView.axis = .vertical
+        itemView.translatesAutoresizingMaskIntoConstraints = false
+        let bidLabel = UILabel()
+        bidLabel.text = "Booking ID: \(booking.bid)"
+        bidLabel.sizeToFit()
+        let movieNameLabel = UILabel()
+        movieNameLabel.text = "Movie: \(booking.movieName)"
+        movieNameLabel.sizeToFit()
+        let seatsLabel = UILabel()
+        let seatsStr = booking.reservedSeats.joined(separator: ", ")
+        seatsLabel.text = "Seats: \(seatsStr)"
+        seatsLabel.sizeToFit()
+        let cancelBookingButton = UIButton()
+        cancelBookingButton.setTitle("Cancel booking", for: .normal)
+        Style.styleFilledButton(cancelBookingButton)
+        cancelBookingButton.layer.cornerRadius = 5
+        cancelBookingButton.layer.name = booking.bid
+        cancelBookingButton.addTarget(self, action: #selector(onCancelBookingTapped(_:)), for: .touchUpInside)
+        itemView.addArrangedSubview(bidLabel)
+        itemView.addArrangedSubview(movieNameLabel)
+        itemView.addArrangedSubview(seatsLabel)
+        itemView.addArrangedSubview(cancelBookingButton)
+        
+        return itemView
+    }
+    
+    @IBAction func onCancelBookingTapped(_ sender: UIButton) {
+        let bidToBeCanceled: String = sender.layer.name!
+        print("Cancel booking tapped: \(bidToBeCanceled)")
+        sender.setTitle("Cancelling...", for: .normal)
+        self.removeBooking(bidToRemove: bidToBeCanceled)
+    }
+    
     
     // TODO: Use this function to remove a booking record from database.
     // NOTE: self.bookings will be updated automatically, if success
     func removeBooking(bidToRemove: String) {
+        guard self.bookings.count > 0 else {
+            print("No booking to remove")
+            return
+        }
+        
         self.db.removeBookingByBid(bid: bidToRemove) { success in
             guard success else {
                 print("ticketViewController - removeBooking - failed")
@@ -69,9 +146,13 @@ class TicketViewController: UIViewController {
     }
     
     
-    /*******************/
-    // Danger Zone: DO NOT use this method, DO NOT modify
-    /*******************/
+    func showAlert(msg: String) {
+        let alert = UIAlertController(title: "Success", message: msg, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
     func removeSeats(bid: String) {
         var bookingToRemove: Booking?
         
@@ -97,13 +178,11 @@ class TicketViewController: UIViewController {
             }
             self.db.updateReservedSeats(mid: mid, reservedSeats: newReservedSeats, { success in
                 /*******************/
-                // IMPORTANT:  update self.bookings after a booking record is removed
+                // IMPORTANT:  reload page after a booking record is removed
                 /*******************/
                 if success {
-                    self.db.getBookingByUid(uid: self.uid) { bookings in
-                        self.bookings = bookings
-                        print("Booking removed, current bookings: \(self.bookings)")
-                    }
+                    self.showAlert(msg: "Booking Removed!")
+                    self.viewDidLoad()
                 }
             })
         }
